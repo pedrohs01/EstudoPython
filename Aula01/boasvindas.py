6a5bbedaade7b4dcb2a453f3bdc2140a420c9285
PEDRO HENRIQUE DOS SANTOS <pedrosantosjovem2004@gmail.com>
2024-02-29 15:29:01 -0300

    novaspy

---
+++ Aula01/boasvindas.py
@@ -0,0 +1,6 @@
+#  vamos criar uma variavel para armazenar o nome do usuario 
+nome = "admin"
+print("Ola, "+nome+". seja bem vindo!")
+print("escreva seu novo nome de usuário:")
+nome = input("digite aqui:")
+print("o seu novo usuário é :"+nome)
