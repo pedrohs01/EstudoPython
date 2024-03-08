e8665f9be10d1cac257c89f2d18c7889a2f3284d
PEDRO HENRIQUE DOS SANTOS <pedrosantosjovem2004@gmail.com>
2024-03-08 15:04:34 -0300

    usando modulos

---
+++ Aula04/usar_funcoes.py
@@ -0,0 +1,8 @@
+# import biblioteca as bb
+from biblioteca import boasvindas,anobissexto,limpa_tela,pontas_lista
+
+num = [10, 25, 88, 84, 25, 12]
+limpa_tela()
+boasvindas()
+print(anobissexto(2023))
+print(pontas_lista(num))
